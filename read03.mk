7be2ea2d341d8a2827bc7aef811a08a81528d842
SuadTalafha <>
2021-04-12 12:49:53 +0000

    save read03

---
+++ read03.mk
@@ -0,0 +1,27 @@
+# Version Control :
+Version Control is a system that allows you to revisit various versions of a file or set of files by recording changes. 
+ ## Types of version control :
+ - Local Version Control
+ - Centralized Version Control
+ - Distributed Version Control
+
+  # what is Git?
+ ## Snapshots
+
+Git is a DVCS that stores data in a file system made up of snapshots. Each time you save a changed version of your project — called commit — Git creates a snapshot of the file and stores a reference to it.
+
+## Local Operations
+
+Git mostly relies on local operations because most necessary information can be found in local resources.
+
+## Tracking Changes
+
+Every single change applied to any file or directory is tracked by Git. And, as the gatekeeper, Git will always detect file corruption or loss of information in transit.
+
+## Loss of Data
+
+Git is set up to greatly minimize the possibility of irreversible damage to files, such as accidentally lost data. Git makes it extremely difficult for a snapshot of your file that is committed to be lost.
+
+## giStates
+
+Files in Git can reside in three main states: committed, modified and staged.
